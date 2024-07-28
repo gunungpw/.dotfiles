f2f75a3128315f3145e09d05dd745d85570a090b
gunungpw <gunungpambudiw@gmail.com>
2024-07-28 22:37:53 +0700

    first commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require "wezterm"
+local config = {}
+
+-- Settings for looks
+config.color_scheme = "Monokai"
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 10.0
+config.font =
+    wezterm.font_with_fallback {
+    "Cascadia Code NF",
+	"Fira Code",
+    --'FiraCode Nerd Font Mono',
+    --'CaskaydiaCove Nerd Font Mono',
+}
+
+-- Default Shell
+config.default_prog = {
+    "nu",
+    "--config C:\\Users\\Acer\\dotfiles\\nushell\\config.nu",
+    "--env-config C:\\Users\\Acer\\dotfiles\\nushell\\env.nu"
+}
+
+config.max_fps = 60
+
+config.initial_cols = 120
+config.initial_rows = 30
+
+return config
